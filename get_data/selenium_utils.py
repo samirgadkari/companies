3037f07bb3852ae3ff278358376a7108adffcf72
Samir Gadkari <samir.gadkari@gmail.com>
2020-01-27 13:55:19 -0800

    Ignore error with selenium if HTML page too large

--- get_data/selenium_utils.py
+++ get_data/selenium_utils.py
@@ -1,8 +1,30 @@
+import os
 import time
 import random
+from decouple import config
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+
+def download_file_options(opts):
+
+    # Value 0 = Save file on users desktop.
+    # Value 1 = Save file in users downloads folder.
+    # Value 2 = Save file to given directory.
+    opts.set_preference("browser.download.folderList", 2)
+
+    # Save the file in this directory.
+    opts.set_preference("browser.download.dir",
+                        config("TEMP_DATA_DIR"));
+
+    # Download manager should not open window when file starts downloading.
+    opts.set_preference("browser.download.manager.showWhenStarting", False)
+
+    # Set the MIME type of file we're downloading.
+    opts.set_preference("browser.helperApps.neverAsk.saveToDisk",
+                        "text/plain")
 
 
 def configure_browser():
@@ -10,8 +32,10 @@ def configure_browser():
     opts.headless = True
 
     browser = Firefox(options=opts)
-    browser.wait = WebDriverWait(browser, 5)  # Wait for 5 seconds
-                                              # on each get() request.
+
+    # Max wait is 5 seconds for page to load on each get() request
+    browser.wait = WebDriverWait(browser, 5)
+
     return browser
 
 
@@ -21,3 +45,33 @@ def browser_sleep(start, stop):
     time.sleep(random.randint(3, 10))
 
 
+# browser.page_source does not work for huge HTML pages.
+# Out HTML pages will be huge sometimes, since it's the text
+# of all the company filing data.
+# Instead,
+#   - download the file to a local directory,
+#   - read it to get the data,
+#   - delete it to make sure disk space is not getting full, and
+#   - return the read data.
+#
+# Found out that this does not work for links.
+# There a way to right-click (context_click),
+# but there is no way to access the browser's
+# context menu.
+# This will only work if you have a context menu
+# inside your HTML page.
+def get_filing(browser, element, filename):
+
+    element.click()
+
+    # Investigate a better way to do this.
+    browser_sleep(10, 10)
+
+    full_filename = os.path.join(config('TEMP_DATA_DIR'), filename)
+    print(f'Getting full_filename: {full_filename}')
+    with open(full_filename, 'r') as f:
+        data = f.read()
+
+    os.remove(full_filename)
+
+    return data
