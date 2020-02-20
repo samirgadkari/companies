ee54f1eed84ed1183b0221d6a3a0a21ec0cbbe75
Samir Gadkari <samir.gadkari@gmail.com>
2020-02-19 16:07:48 -0800

    Split data into useable and not useable list of files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from get_data.get_10Ks_selenium import get_10Ks
 from get_data.get_companies_in_sector import get_companies
 from extract_data.extract_tables import extract_tables
+from extract_data.filter import filter_data
 
 if __name__ == '__main__':
     try:
@@ -12,7 +13,8 @@ if __name__ == '__main__':
     switcher = {
         'extract_tables': extract_tables,
         'get_10Ks':       get_10Ks,
-        'get_companies':  get_companies
+        'get_companies':  get_companies,
+        'filter':         filter_data,
     }
     func = switcher.get(function_, lambda: "nothing")
     func()
