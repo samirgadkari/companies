ca14323e7a62051668f478b8a2771c09446399f3
Samir Gadkari <samir.gadkari@gmail.com>
2020-02-24 12:58:33 -0800

    Code to check hand-created text tables to json

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from get_data.get_10Ks_selenium import get_10Ks
 from get_data.get_companies_in_sector import get_companies
 from extract_data.extract_tables import extract_tables
 from extract_data.filter import filter_data
+from extract_data.text_to_json import check_hand_created_samples
 
 if __name__ == '__main__':
     try:
@@ -12,6 +13,7 @@ if __name__ == '__main__':
                          '<"extract_tables|get_10Ks|get_companies>"')
     switcher = {
         'extract_tables': extract_tables,
+        'check_hand_created_samples': check_hand_created_samples,
         'get_10Ks':       get_10Ks,
         'get_companies':  get_companies,
         'filter':         filter_data,
