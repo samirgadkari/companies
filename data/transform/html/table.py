cfd886c12a38b99cf5b0bce610c66af14c4d6475
Samir Gadkari <samir.gadkari@gmail.com>
2020-04-01 11:53:25 -0700

    Refactor: Start using Pandas Dataframe with BeautifulSoup data

--- data/transform/html/table.py
+++ data/transform/html/table.py
@@ -1,5 +1,9 @@
+import sys
+import re
 from utils.file import read_file
 from bs4 import BeautifulSoup
+import numpy as np
+import pandas as pd
 from .cell import Cell  # text_align_attr
 from .row import Row
 
@@ -20,12 +24,58 @@ class Table():
         self.shorten()
         self.rows = self.add_rows()
 
+        self.df = self.create_dataframe()
+        self.update_dataframe()
+        with pd.option_context('display.max_rows', None,
+                               'display.max_columns', None):
+            print(self.df)
+
+        sys.exit(0)
+
 
     def add_rows(self):
         rows = []
         for row_tag in self.table_tag.find_all('tr'):
             rows.append(Row(row_tag))
         return rows
+
+
+    def update_dataframe(self):
+        df = self.df
+
+        def update_alignment(cell):
+            matches = Cell.text_align_attr.search(cell['style'])
+            if matches is None:
+                return Cell.TEXT_ALIGN_LEFT
+            else:
+                return Cell.alignment_mapping[matches.group(1)]
+
+        df['alignment'] = df['cell'].apply(lambda cell: update_alignment(cell))
+
+        def update_span(cell, type_):
+            try:
+                return int(cell[type_])
+            except KeyError as e:
+                return 1
+
+        df['rowspan'] = df['cell'].apply(lambda cell: update_span(cell, 'rowspan'))
+        df['colspan'] = df['cell'].apply(lambda cell: update_span(cell, 'colspan'))
+
+        df['text'] = df['cell'].apply(lambda cell: str(cell)) \
+                               .str.replace('\xa0', '', flags=re.MULTILINE) \
+                               .str.replace('\n', '', flags=re.MULTILINE)
+
+
+    def create_dataframe(self):
+        # import pdb; pdb.set_trace()
+        df_input = {'row': [], 'cell': []}
+        idx = 0
+        for row in self.rows:
+            for cell in row.cells:
+                df_input['row'].append(row.row_tag)
+                df_input['cell'].append(cell.cell_tag)
+        return pd.DataFrame(data=df_input)
+
 
     def shorten(self):
         self.tags()
