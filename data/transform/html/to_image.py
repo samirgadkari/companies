ecd079eb6f9736ccae5e0ffcaa20221a10ff8cd9
Samir Gadkari <samir.gadkari@gmail.com>
2020-06-07 11:03:15 -0700

    Added comments

--- data/transform/html/to_image.py
+++ data/transform/html/to_image.py
@@ -1,3 +1,11 @@
+'''
+Convert the HTML file that contains a single table tag
+into a large PNG file with large font sizes.
+The size required is large since
+OCR (Optical Character Recognition) is easier with
+larger font sizes. The large file size gives ample
+space between word groups.
+'''
 import re
 import pdfkit
 from bs4 import BeautifulSoup
@@ -5,11 +13,20 @@ from pdf2image import convert_from_path
 
 
 def remove_escaped_strings(raw_html):
+    '''
+    Remove escaped strings.
+    They don't contain any information we need.
+    '''
     escaped_str_regex = re.compile(r'&#\d+;')
     return escaped_str_regex.sub('', raw_html)
 
 
 def remove_superscripts(table_tag):
+    '''
+    Remove superscripts.
+    They don't contain any information we need,
+    and are not easy to do OCR with.
+    '''
     sups = table_tag.find_all('sup')
     for sup in sups:
         sup.decompose()
@@ -17,6 +34,9 @@ def remove_superscripts(table_tag):
 
 
 def html_to_image(filename):
+    '''
+    Convert the given HTML file into a PNG file.
+    '''
     with open(filename, 'r') as f:
 
         raw_html = f.read()
@@ -25,11 +45,14 @@ def html_to_image(filename):
         table_tag = BeautifulSoup(clean_html, 'html.parser')
         table_tag = remove_superscripts(table_tag)
 
-        # Change the table style to:
+        # Change the table style to the one given below.
+        # This style is good for OCR (Optical Character Recognition)
+        # which we will do after generating the image.
         style = "font-family:Lucida Console;font-size:96pt;margin-left:auto;" \
             "margin-right:auto;width:100%;border:collapse;text-align:left;"
         table_tag['style'] = style
 
+        # Convert the html tag into pdf.
         # Specify a large page size, so our PDF is huge.
         # A large PDF is required for
         # better OCR (Optical Character Recognition)
@@ -37,8 +60,8 @@ def html_to_image(filename):
                            'out.pdf',
                            options={'page-size': 'A2'})
 
+        # Save the pdf file as a png file.
         images = convert_from_path('out.pdf')
-        # import pdb; pdb.set_trace()
         images[0].save('out.png', 'PNG')
 
 
