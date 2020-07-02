9fc23a497ba202a544ff46263b6b9e4428c42338
Samir Gadkari <samir.gadkari@gmail.com>
2020-07-02 15:25:00 -0700

    PEP8 compatibility changes

--- utils/html.py
+++ utils/html.py
@@ -1,12 +1,12 @@
 import re
-import copy
-from bs4 import BeautifulSoup
 
 regex_html_tag = re.compile(r'<[^>]+>')
 regex_html_tag_or_data = re.compile(r'(<[^]]+>)|([^<]+)')
 
+
 def replace_html_tags(text):
     return regex_html_tag.sub(' ', text)
+
 
 def next_tag_or_data(data):
     item = regex_html_tag_or_data.search(data)
